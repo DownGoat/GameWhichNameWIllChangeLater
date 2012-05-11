637bb968d13e05e7e598d7dccbdc8f31870763be
Sindre Smistad <nfomaker@gmail.com>
2012-05-12 00:04:34 +0100

    sdf

--- SomeGame/src/game/entities/Entity.java
+++ SomeGame/src/game/entities/Entity.java
@@ -1,5 +1,37 @@
 package game.entities;
 
-public interface Entity {
+import org.newdawn.slick.geom.Rectangle;
 
+public interface Entity {
+	/**
+	 * Draws the GameObject to the screen. 
+	 */
+	void draw();
+
+	/**
+	 * 
+	 * @return X coordinate.
+	 */
+	float getX();
+
+	/**
+	 * 
+	 * @return Y Coordinate.
+	 */
+	float getY();
+
+	int getWidth();
+
+	int getHeight();
+
+	/**
+	 * Updates the GameObjects position on the screen.
+	 * @param x The X coordinate.
+	 * @param y The Y coordinate.
+	 */
+	void updatePosition(float x, float y);
+
+	boolean intersects(Entity ent);
+
+	Rectangle getRect();
 }
